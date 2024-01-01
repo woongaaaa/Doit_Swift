46bbcb57eb1eedd784d25cfd2c864bfff14b72f3
woongaaaa <woonge0930@naver.com>
2024-01-01 21:11:17 +0900

    03장 미션 Image Viewer

--- Doit_Swift/ViewController.swift
+++ Doit_Swift/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number : Int = 1
+    
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
+        
+        imgView.image = UIImage(named: String(number))
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func prevBtn(_ sender: UIButton) {
+        if (number > 1){
+            number -= 1
+        }
+        imgView.image = UIImage(named: String(number))
+    }
+    @IBAction func nextBtn(_ sender: UIButton) {
+        if (number < 6){
+            number += 1
+        }
+        imgView.image = UIImage(named: String(number))
+    }
+        
+        
 }
-
